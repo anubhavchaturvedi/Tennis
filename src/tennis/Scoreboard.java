e9e73a2f6680b8f03199c784ccf0401b4b5cb2b3
Anubhav Chaturvedi <mailforanubhav@gmail.com>
2015-07-27 10:42:15 +0530

    boilerplate code

---
+++ src/tennis/Scoreboard.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tennis;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author test
+ */
+public class Scoreboard {
+    private ArrayList<Integer> player1;
+    private ArrayList<Integer> player2;
+    private String player1name;
+    private String player2name;
+    
+    Scoreboard(){
+        player1name = "D";
+        player2name = "F";
+    }
+    
+    boolean updateScore(String playerScored) {
+        
+    }
+    
+    
+    boolean hasGameEnded() {
+        
+    }
+    
+    String currentScore() {
+        
+    }
+    
+    void displayScoreboard(){
+        
+    }
+    
+}
